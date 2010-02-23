c5e27bd749e9e0d6e65de9621def855b16c84a94
lestrrat <lestrrat+github@gmail.com>
2010-02-23 12:12:11 +0900

    version++

--- lib/AnyEvent/HTTP/MXHR.pm
+++ lib/AnyEvent/HTTP/MXHR.pm
@@ -4,7 +4,7 @@ use AnyEvent::HTTP;
 use AnyEvent::Util qw(guard);
 use base qw(Exporter);
 our @EXPORT = qw(mxhr_get);
-our $VERSION = '0.00003';
+our $VERSION = '0.00004';
 
 sub mxhr_get ($@) {
     my $cb = pop;
